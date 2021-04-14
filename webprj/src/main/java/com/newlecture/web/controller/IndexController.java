0d95d4d2d0cd40ab12b77d6e96804c800393b51d
goodchuck <79144494+goodchuck@users.noreply.github.com>
2021-04-14 20:08:17 +0900

    21/04/14 Spring MVC 27강 수강 - Annotation으로 URL 매핑하기

--- webprj/src/main/java/com/newlecture/web/controller/IndexController.java
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -3,20 +3,29 @@ package com.newlecture.web.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+//import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller{
+@Controller
+public class IndexController{
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		//System.out.println("index controller");
-		//ModelAndView mv = new ModelAndView();
-		ModelAndView mv = new ModelAndView("root.index");
-		mv.addObject("data", "Hello Spring MVC~");
-		//mv.setViewName("/WEB-INF/view/index.jsp");
-		return mv;
+	@RequestMapping("/index")
+	public void aaaa() {
+		System.out.println("asdfasdfsadf");
 	}
+	
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		// TODO Auto-generated method stub
+//		//System.out.println("index controller");
+//		//ModelAndView mv = new ModelAndView();
+//		ModelAndView mv = new ModelAndView("root.index");
+//		mv.addObject("data", "Hello Spring MVC~");
+//		//mv.setViewName("/WEB-INF/view/index.jsp");
+//		return mv;
+//	}
 	
 }
