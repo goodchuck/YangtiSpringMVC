1cc9187269b30c6621e353bbbe172ba25953fb7b
goodchuck <79144494+goodchuck@users.noreply.github.com>
2021-04-15 13:24:28 +0900

    21/04/15 Spring MVC 31강 수강 - 문서 출력방법 4가지 @ResponseBody

--- webprj/src/main/java/com/newlecture/web/controller/HomeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.servlet.mvc.Controller;
 
@@ -13,13 +17,31 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class HomeController{
 
-	@RequestMapping("/index")
+//	@RequestMapping("/index")
+//	public String index() {
+//		
+//		return "root.index"; //타일즈에서 리절빙이 가능해짐
+//	}
+	
+//	@RequestMapping("index")
+//	public void index(HttpServletResponse response) {
+//		PrintWriter out;
+//		try {
+//			out = response.getWriter();
+//			out.println("Hello Index");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		//return "root.index"; //타일즈에서 리절빙이 가능해짐
+//	}
+	
+	@RequestMapping("index")
+	@ResponseBody 	// return하는 녀석은 자체 String이다
 	public String index() {
 		
-		return "root.index"; //타일즈에서 리절빙이 가능해짐
-	}
-	
-	
+		return "Hello Index 1"; 
+	}	
 	
 //	@Override
 //	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
